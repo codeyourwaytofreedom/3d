39bd7c641547aae18e5efd435b0da888d9ec2a3d
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-27 19:33:43 +0300

    Pan & zoom options tested. Works well.
    Item customisation tested.
    Works well.
    
    Things are getting pretty.
    
    Keep going.
    
    Remember why you started.

--- public/Shoe.js
+++ public/Shoe.js
@@ -6,13 +6,13 @@ export default function Model({ ...props }) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/shoe.gltf')
   return (
-    <group ref={group} {...props} dispose={null} scale={6}>
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+    <group ref={group} {...props} dispose={null} scale={3}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={"gold"}/>
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={"black"}/>
       <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
       <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={"gold"}/>
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={"gold"}/>
       <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
       <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
     </group>
