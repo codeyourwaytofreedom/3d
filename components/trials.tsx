e9c23aca24600267c0858ea737df626ccbc86704
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-28 14:11:55 +0300

    6 new properties tested.
    Understood the basic logic.
    
    Already have a 3D home page design in my head.
    
    Keep going.
    
    Remember why you started.

--- components/trials.tsx
+++ components/trials.tsx
@@ -13,15 +13,22 @@ const Trials = () => {
             <div className={t.trials_canvas}>
                 <Canvas className={t.canvas} >
                     <Suspense fallback={null}>
+                    {/* <color attach="background" args={['#202025']} /> */}
                         <ambientLight intensity={0.1} />
                         <Model/>
-                        <spotLight intensity={0.3} angle={0.9} penumbra={1} position={[10,15,10]} castShadow/>
-                        <OrbitControls enablePan={true}/>
+                        <spotLight intensity={0.5} angle={0.9} penumbra={1} position={[10,15,10]} castShadow/>
+                        <OrbitControls 
+                            enablePan={false} 
+                            maxDistance={15} 
+                            minDistance={10} 
+                            target={[3,0,0]} 
+                            minPolarAngle={Math.PI/2}
+                            maxPolarAngle={Math.PI/2}
+                        
+                        />
                     </Suspense>
                 </Canvas>
             </div>
-               
-            
         </div>
      );
 }
